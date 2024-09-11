d24389a52be1dbc68f2e43769c525c4f28be6c7e
Ronald Hohls <ronhohls@gmail.com>
2024-09-11 20:57:13 +0200

    Add the Status Table and related CRUD operation pages

---
+++ DocumentRegister.Infrastructure/Persistence/Configurations/StatusConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DocumentRegister.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentRegister.Infrastructure.Persistence.Configurations
+{
+    public class StatusConfiguration : IEntityTypeConfiguration<Status>
+    {
+        public void Configure(EntityTypeBuilder<Status> builder)
+        {
+            builder.HasData(
+                new Status
+                {
+                    StatusId = 1,
+                    Description = "TestValue",
+                    DateCreated = DateTime.Now,
+                    DateModified = DateTime.Now
+                });
+        }
+    }
+}
---
+++ DocumentRegister.Infrastructure/Persistence/Repositories/StatusRepository.cs
@@ -0,0 +1,14 @@
+﻿using DocumentRegister.Application.Contracts.Persistence;
+using DocumentRegister.Core.Entities;
+using DocumentRegister.Infrastructure.Persistence.Context;
+
+
+namespace DocumentRegister.Infrastructure.Persistence.Repositories
+{
+    public class StatusRepository : GenericRepository<Status>, IStatusRepository
+    {
+        public StatusRepository(DocumentRegisterDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
